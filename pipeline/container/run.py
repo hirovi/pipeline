4d6b25357143181a2615efe8d523ca9d4df1749e
YvanBuggy <yvan@getneuro.ai>
2023-10-30 12:36:43 +0000

    serve basic react from a fastapi route

--- pipeline/container/run.py
+++ pipeline/container/run.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 
 from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 
 from pipeline.cloud.schemas import runs as run_schemas
 from pipeline.container.manager import Manager
@@ -31,3 +32,38 @@ async def execution_handler(execution_queue: asyncio.Queue, manager: Manager) ->
             response_queue.put_nowait(output)
         except Exception:
             logger.exception("Got an error in the execution loop handler")
+
+
+@router.get("/form", tags=["run"], response_class=HTMLResponse)
+async def get_pipeline_form():
+    import os
+
+    with open(os.path.join("/app/pipeline/container", "app.tsx"), "r") as file:
+        ts_code = file.read()
+    return f"""
+    <!-- Your code that needs compiling goes in a type="text/babel" `script` tag -->
+    <script type="text/babel" data-presets="react,stage-3">
+    {ts_code}
+
+    ReactDOM.render(<App />, document.getElementById("root"));
+    </script>
+
+    <div id="root"></div>
+
+    <!-- This is what supports JSX compilation (and other transformations) -->
+
+    <script src="https://unpkg.com/@babel/standalone@7.10.3/babel.min.js"></script>
+
+    <link
+        href="https://cdn.jsdelivr.net/npm/tailwindcss@2/dist/tailwind.min.css"
+        rel="stylesheet"
+    >
+
+    <!-- These are for React -->
+    <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/react/17.0.2/umd/react.development.js"
+    ></script>
+    <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/react-dom/17.0.2/umd/react-dom.development.js"
+    ></script>
+    """
